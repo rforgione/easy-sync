183ade21db7541aa485dee155c0770f66082ff44
rforgione <rforgione@etsy.com>
2017-05-26 14:35:59 -0400

    remove python install script

--- easy_sync_install.py
+++
@@ -1,6 +0,0 @@
-from shutil import copyfile
-import subprocess
-from utilities import run_shell_cmd
-
-copyfile("easy_sync.py", "~/bin/easy_sync.py")
-subprocess.call("chmod +x ~/bin/easy_sync.py", shell=True)
